f9188391a994b11c7e0ec54e09ac668f672af3f7
unlikelyUsual <kumarprabhat308@gmail.com>
2025-08-09 20:40:20 +0530

    Connection string return

--- app/main.js
+++ app/main.js
@@ -5,7 +5,12 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.on("data", (data) => {
+    console.log(`Received from client: ${data.toString()}`);
+    connection.write("pong: " + data.toString());
+  });
+
+  console.log("Client connected.");
 });
 
 server.listen(6379, "127.0.0.1");
