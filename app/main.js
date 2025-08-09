3d71a0b0e3b277592dfa9b16b09e0890c4566eb1
unlikelyUsual <kumarprabhat308@gmail.com>
2025-08-09 20:52:33 +0530

    Multiple ping response

--- app/main.js
+++ app/main.js
@@ -6,11 +6,8 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
-    console.log(`Received from client: ${data.toString()}`);
     connection.write(`+PONG\r\n`);
   });
-
-  console.log("Client connected.");
 });
 
 server.listen(6379, "127.0.0.1");
